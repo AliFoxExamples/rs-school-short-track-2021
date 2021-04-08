d7bb7c7b9b679a886cd908afb289ec7d3b460b52
AliFoxExamples <user-ali-sadykov@yandex.ru>
2021-04-09 00:42:43 +0300

    feat: solve 5th task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,16 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const numArr = `${n}`.split('');
+  function finder(arr, i = 0) {
+    if (i === arr.length) {
+      return 0;
+    }
+    const arr2 = arr.filter((el, k) => k !== i);
+    return Math.max(+arr2.join(''), finder(arr, i + 1));
+  }
+  return finder(numArr);
 }
 
 module.exports = deleteDigit;
